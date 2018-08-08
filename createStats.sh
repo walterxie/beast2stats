5fb9c17e69ee18ba7f8667df01441ceefc6d2723
Walter Xie <walterxie@users.noreply.github.com>
2018-08-08 12:13:12 +1200

    add script

---
+++ createStats.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+### tested in MacOS Sierra 10.12.6
+# chmod u+x 
+
+# dir containing all .txt for analysis
+WD="$HOME/WorkSpace/beast2stats"
+# dir containing all checked out packages
+WD_PKG="$HOME/WorkSpace"
+# MONTHS = number of months expected - 1, e.g. MONTHS=59 for 5 years
+MONTHS=2
+
+PACKAGE="beast2"
+
+### init
+TMP="tmp-$(date +%Y-%m-%d)"
+cd $WD
+if [[ ! -e $TMP ]]; then
+    mkdir $TMP
+    echo "Create $WD/$TMP to store *.txt"
+else 
+    cd $TMP
+    echo "Clean *.txt in $WD/$TMP ..."
+    rm *.txt
+fi
+
+### analyse package
+cd $WD_PKG
+if [[ ! -e $PACKAGE ]]; then
+    echo "$PACKAGE does not exist in $WD_PKG !"
+    exit 1
+else 
+    cd $PACKAGE
+    echo "Working dir is $PWD now, analysing package $PACKAGE ..."
+fi
+
+# update
+git pull
+git checkout origin/master
+
+# loop through n months before 
+for m_before in $(seq 0 $MONTHS);
+do
+    # e.g. 2018-07-01
+    DATE_CHECK_OUT=`date -v-${m_before}m +%Y-%m-01`
+    # checkout version at the 1st date of a month
+    git checkout `git rev-list -n 1 --before="$DATE_CHECK_OUT 00:00" master`
+    # count the line of java code 
+    git ls-files | grep "\.java$" | xargs wc -l > "$PACKAGE-$DATE_CHECK_OUT.txt"
+    
+    # mv txt to tmp dir created previously
+    mv "$PACKAGE-$DATE_CHECK_OUT.txt" "$WD/$TMP"
+done
+
+# make sure HEAD back to origin/master
+git checkout origin/master
+
+# back to WD of all packages
+cd $WD_PKG
+
